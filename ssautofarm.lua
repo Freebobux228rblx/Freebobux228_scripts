0243bb19974f36b8f8ebe7c158e937edd956b0bd
Freebob_ux228 <90867201+Freebobux228rblx@users.noreply.github.com>
2021-09-23 18:07:15 +0300

    1.0.0

---
+++ ssautofarm.lua
@@ -0,0 +1,51 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(19.704967498779, 112.3669128418, -71.008422851563)
+game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm";
+            Text = "Made by Freebob_ux228#1814 And HtB#1526";
+                    })
+                 wait(3.3)
+--Start in 8sec
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(253.1178894043, 3.6678714752197, 11.376895904541)
+game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm";
+            Text = "Getting cords...";
+                    })
+                 wait(4.9)
+                 game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm";
+            Text = "Cords getted. Starting First location...";
+                    })
+                 wait(4.9)
+                 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Freebobux228rblx/Freebobux228_scripts/main/ssautofarm-firstlocation.lua",true))()
+wait(1.1)
+--Get second
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-614.29956054688, 3.5758934020996, 29.962440490723)
+wait(0.01)
+ game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm.";
+            Text = "Getting Territory #2...";
+                    })
+                 wait(3.3)
+                 game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm.";
+            Text = "Starting Territory #2 autofarm...";
+                    })
+                 wait(3.8)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Freebobux228rblx/Freebobux228_scripts/main/ssautofarm-secondlocation.lua",true))()
+--Myst box autofarm
+game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm";
+            Text = "Myst`s box tp In 2-4 sec";
+                    })
+                 wait(3.1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(214.99499511719, 4.5258936882019, 44.083065032959)
+wait(0.01)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-358.52059936523, 4.4758920669556, 11.717916488647)
+--Tp to tom
+game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Ss autofarm";
+            Text = "TP In 2-4 sec";
+                    })
+                 wait(3.3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3.2574551105499, 3.4758925437927, -117.68488311768)
