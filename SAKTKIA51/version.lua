b82c81a4ff93d7e1455da9c5e3cb5b22dd57a249
Freebob_ux228 <90867201+Freebobux228rblx@users.noreply.github.com>
2023-10-19 21:16:11 +0300

    Update version.lua

--- SAKTKIA51/version.lua
+++ SAKTKIA51/version.lua
@@ -2,5 +2,6 @@ return {
 ["LatestVersion"] = "1b1b4",
 ["LatestVersionKavoUI"] = "1b1b4",
 ["Domain"]="no",
-["IS_SAKTKIA51_PATCHED"]=false
+["IS_SAKTKIA51_PATCHED"]=false,
+["SCRIPT_LINK"]="https://ify.ac/diV"
 }
