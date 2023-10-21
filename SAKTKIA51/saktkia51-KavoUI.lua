4a873425adf7d9fd11f7791b0d65195360e0be31
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2023-10-22 01:28:49 +0300

    nah uh
    freebobux u forgot to call return from coroutine.wrap()

--- SAKTKIA51/saktkia51-KavoUI.lua
+++ SAKTKIA51/saktkia51-KavoUI.lua
@@ -1,2 +1,2 @@
 game.StarterGui:SetCore("ChatMakeSystemMessage", {Text = "Thanks for ur using SAKTKIA51//Kavo, that time was cool.. now it discountied until further notice";Color = Color3.fromRGB(255,0,0);TextSize = 20})
-coroutine.wrap(loadstring(game:HttpGet("https://raw.githubusercontent.com/Freebobux228rblx/Freebobux228_scripts/main/SAKTKIA51/SAKTKIA51.lua")))
+coroutine.wrap(loadstring(game:HttpGet("https://raw.githubusercontent.com/Freebobux228rblx/Freebobux228_scripts/main/SAKTKIA51/SAKTKIA51.lua")))()
