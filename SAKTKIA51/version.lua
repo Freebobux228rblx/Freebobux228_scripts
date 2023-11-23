f5f88cc77d4544979d9dd6097f75e7e79f20eb03
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2023-11-23 19:42:48 +0300

    Update version.lua

--- SAKTKIA51/version.lua
+++ SAKTKIA51/version.lua
@@ -1,5 +1,5 @@
 return {
-["LatestVersion"] = "1c2c0",
+["LatestVersion"] = "1c2c1",
 ["LatestVersionKavoUI"] = "1b1b4",
 ["Domain"]="no",
 ["ach"]="8d8105ffa98be522d6f66d7d6698122730254358ff2d7267bd1d3f7074a908f43a66a0117488aae5b000efa7234b1df8",
