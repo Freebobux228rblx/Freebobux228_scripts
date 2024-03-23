fe95bcc77d5ff22b18551d50be8728753fe8e5c3
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2024-03-23 13:51:59 +0300

    nah

--- SAKTKIA51/version.lua
+++ SAKTKIA51/version.lua
@@ -3,6 +3,6 @@ return {
 ["LatestVersionKavoUI"] = "1b1b4",
 ["Domain"]="no",
 ["ach"]="8d8105ffa98be522d6f66d7d6698122730254358ff2d7267bd1d3f7074a908f43a66a0117488aae5b000efa7234b1df8",
-["IS_SAKTKIA51_PATCHED"]=false,
+["IS_SAKTKIA51_PATCHED"]=true,
 ["SCRIPT_LINK"]="https://ify.ac/diV"
 }
