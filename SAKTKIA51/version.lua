f7bc0cbf073a035a22ec510f3e3a660004621273
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2024-12-29 22:55:16 +0300

    Update version.lua

--- SAKTKIA51/version.lua
+++ SAKTKIA51/version.lua
@@ -2,7 +2,7 @@ return{
 ["LatestVersion"] = "1c2c2",
 ["LatestVersionKavoUI"] = "1b1b4",
 ["Domain"]="no",
-["ach"]="1a8df7f558a81148f3146cc6d7cae52bd341b5461caf0d21d37291a29f96ad2796bc957bcd8e88ca79f5618af0b272f9",
+["ach"]="b1e810a4266d02fde407899e8026d323b4aad8f22fda80e541a35d56d5897d844a5a855122d44218f9706c429c199e4e",
 ["IS_SAKTKIA51_PATCHED"]=false,
 ["SCRIPT_LINK"]="https://ify.ac/diV"
 }
