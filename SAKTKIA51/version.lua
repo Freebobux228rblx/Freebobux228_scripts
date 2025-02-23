ebc5f5e47a329960acfe6304f98c1d13d61d8bf2
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2025-02-23 20:15:25 +0300

    cmon update

--- SAKTKIA51/version.lua
+++ SAKTKIA51/version.lua
@@ -3,6 +3,6 @@ return{
 ["LatestVersionKavoUI"] = "1b1b4",
 ["Domain"]="no",
 ["ach"]="b1e810a4266d02fde407899e8026d323b4aad8f22fda80e541a35d56d5897d844a5a855122d44218f9706c429c199e4e",
-["IS_SAKTKIA51_PATCHED"]=false,
+["IS_SAKTKIA51_PATCHED"]=true,
 ["SCRIPT_LINK"]="https://ify.ac/diV"
 }
